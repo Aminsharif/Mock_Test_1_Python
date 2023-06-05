0260026a0469369ac1923ff3c8326bf0e285ba71
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2023-06-05 14:53:54 +0600

    Update First_Unique_Character.py

--- First_Unique_Character.py
+++ First_Unique_Character.py
@@ -1,1 +1,20 @@
+def firstUniqChar(s):
+    char_count = {}
+    
+    # Count the frequency of each character
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+    
+    # Find the first non-repeating character and return its index
+    for i, char in enumerate(s):
+        if char_count[char] == 1:
+            return i
+    
+    # If no non-repeating character is found, return -1
+    return -1
+  
+  
+s = "loveleetcode"
+print(firstUniqChar(s))
 
+ 
