8a2a13f44318204d91ce7d310183246d4f96e557
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2023-06-05 14:41:23 +0600

    Update MoveZeroes.py

--- MoveZeroes.py
+++ MoveZeroes.py
@@ -1,1 +1,15 @@
-
+def moveZeroes(nums):
+    def move(nums, i):
+        if i >= len(nums):
+            return nums
+        
+        if nums[i] == 0:
+            nums.append(nums.pop(i))  # Move the zero to the end of the array
+        
+        return move(nums, i + 1)
+    
+    return move(nums, 0)
+  
+  
+nums = [0,1,0,3,12]
+moveZeroes(nums)
